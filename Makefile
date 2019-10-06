0b6c3ad8a84b43f8bf259045f02b6c214684c800
Daniel Schwartz <mrdschwartz@gmail.com>
2019-10-06 14:08:25 -0700

    Implement most basic template generation

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 test: ctemplate
-	./ctemplate tests/hello.mustache
+	./ctemplate tests/hello.mustache > tests/out.c
+	$(CC) -o tests/out $(CFLAGS) $(LDFLAGS) -lsqlite3 tests/out.c
+	./tests/out  tests/test.db
+	rm tests/out.c tests/out
 
 ctemplate: *.c
-	$(CC) -o ctemplate $(C_FLAGS) $(LD_FLAGS) *.c
+	$(CC) -o ctemplate $(CFLAGS) $(LDFLAGS) *.c
+
+clean:
+	rm ctemplate
--- ctemplate.c
+++ ctemplate.c
@@ -8,6 +8,60 @@
 #define BUF_APPEND(buf,c) do{assert(buf##_n<BUF_SIZE); (buf)[buf##_n++] = (c);}while(0)
 #define error(errmsg, ...) do{ fprintf(stderr, "ERROR: " errmsg ,##__VA_ARGS__); exit(1); }while(0)
 
+void print_preamble(void) {
+    puts(
+            "#include <stdio.h>\n"
+            "#include <stdlib.h>\n"
+            "#include <strings.h>\n"
+            "#include <sqlite3.h>\n"
+            "\n"
+            "#define SQL_MAX 1024\n"
+            "#define error(errmsg, ...) do{ fprintf(stderr, \"ERROR: \" errmsg ,##__VA_ARGS__); exit(1); }while(0)\n"
+            "\n"
+            "sqlite3 *db;\n"
+            "\n"
+            "char **select(char *sel, size_t n){\n"
+            "   sqlite3_stmt *res;\n"
+            "   char sql[SQL_MAX];\n"
+            "   snprintf(sql, SQL_MAX, \"SELECT %s FROM users\", sel);\n"
+            "\n"
+            "   int rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);\n"
+            "   if (rc != SQLITE_OK) \n"
+            "       error(\"Failed to execute statement: %s\\n\", sqlite3_errmsg(db));\n"
+            "\n"
+            "   int step = sqlite3_step(res);\n"
+            "   if (step != SQLITE_ROW) return NULL;\n"
+            "\n"
+            "   char **vals = malloc(sizeof(*vals) * n);\n"
+            "   if(!vals) return NULL;\n"
+            "\n"
+            "   for(size_t i = 0; i < n; i++)\n"
+            "       vals[i] = strdup((const char *)sqlite3_column_text(res, i));\n"
+            "\n"
+            "   sqlite3_finalize(res);\n"
+            "   return vals;\n"
+            "}\n"
+            "\n"
+            "int main(int argc, char **argv){\n"
+            "   char *err_msg = 0;\n"
+            "\n"
+            "   int rc = sqlite3_open(argv[1], &db);\n"
+            "   if (rc != SQLITE_OK)\n"
+            "       error(\"Cannot open database: %s\\n\", sqlite3_errmsg(db));\n"
+            "\n"
+            "   char **vals;\n"
+          );
+}
+
+void print_final(void) {
+    puts(
+            "   sqlite3_close(db);\n"
+
+            "   return 0;\n"
+            "}\n"
+          );
+}
+
 int match_double(int i, char c, FILE *f) {
     if(i == c) {
         i = fgetc(f);
@@ -45,7 +99,7 @@ void fmt_dump(void) {
             if(i > 0) printf(", ");
             printf("%s", fmt_param[i]);
         }
-        printf("\");\n");
+        printf("\", %lu);\n", fmt_param_n);
     }
     printf("printf(\"%.*s\"", (int)fmt_n, fmt);
     for(size_t i = 0; i < fmt_param_n; i++)
@@ -63,6 +117,8 @@ int main(int argc, char **argv) {
     size_t select_buf_n = 0;
     enum {START, SELECT} state = START;
     int c;
+
+    print_preamble();
 
     while(c = fgetc(template), c != EOF) {
         switch(state) {
@@ -95,6 +151,8 @@ int main(int argc, char **argv) {
     if(state != START) error("Unmatched {{");
 
     fmt_dump();
+
+    print_final();
 
     return 0;
 }
