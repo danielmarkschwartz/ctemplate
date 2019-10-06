a24de99c33f4969a0961d569793ac2200043fc2f
Daniel Schwartz <mrdschwartz@gmail.com>
2019-10-06 13:18:24 -0700

    Implement printf generation for select tags

--- ctemplate.c
+++ ctemplate.c
@@ -1,10 +1,11 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
 
 #define BUF_SIZE 1024
-#define BUF_APPEND(buf,c) do{assert(buf##_n<BUF_SIZE); (buf)[buf##_n++] = (char)(c);}while(0)
+#define BUF_APPEND(buf,c) do{assert(buf##_n<BUF_SIZE); (buf)[buf##_n++] = (c);}while(0)
 #define error(errmsg, ...) do{ fprintf(stderr, "ERROR: " errmsg ,##__VA_ARGS__); exit(1); }while(0)
 
 int match_double(int i, char c, FILE *f) {
@@ -17,15 +18,49 @@ int match_double(int i, char c, FILE *f) {
     return i;
 }
 
+char fmt[BUF_SIZE], *fmt_param[BUF_SIZE];
+size_t fmt_n = 0, fmt_param_n = 0;
+
+void fmt_append_str(char *s);
+
+void fmt_append(char c) {
+    if(c == '\n') fmt_append_str("\\n");
+    else BUF_APPEND(fmt,c);
+}
+
+void fmt_append_str(char *s) {
+    assert(s);
+    while(*s) fmt_append(*(s++));
+}
+
+void fmt_append_param(char *s) {
+    assert(s);
+    BUF_APPEND(fmt_param, s);
+}
+
+void fmt_dump(void) {
+    if(fmt_param_n > 0) {
+        printf("vals = select(\"");
+        for(size_t i = 0; i < fmt_param_n; i++) {
+            if(i > 0) printf(", ");
+            printf("%s", fmt_param[i]);
+        }
+        printf("\");\n");
+    }
+    printf("printf(\"%.*s\"", (int)fmt_n, fmt);
+    for(size_t i = 0; i < fmt_param_n; i++)
+        printf(", vals[%lu]", i);
+    printf(");\n");
+}
+
 int main(int argc, char **argv) {
     if(argc != 2) error("Expected one argument: ctemplate <template>");
 
     FILE *template = fopen(argv[1], "r");
     if(!template) error("Couldn't open template file \"%s\" for reading\n", argv[1]);
 
-    char print_buf[BUF_SIZE];
     char select_buf[BUF_SIZE];
-    size_t print_buf_n = 0, select_buf_n = 0;
+    size_t select_buf_n = 0;
     enum {START, SELECT} state = START;
     int c;
 
@@ -34,21 +69,19 @@ int main(int argc, char **argv) {
         case START:
             if((c = match_double(c, '{', template)) == 0) {
                 state = SELECT;
-                BUF_APPEND(print_buf, '\0');
-                printf("print \"%s\"\n", print_buf);
-                print_buf_n = 0;
                 break;
             }
 
-            BUF_APPEND(print_buf, c);
+            fmt_append(c);
             break;
 
         case SELECT:
             if((c = match_double(c, '}', template)) == 0) {
                 state = START;
                 BUF_APPEND(select_buf, '\0');
-                printf("select \"%s\"\n", select_buf);
+                fmt_append_param(strdup(select_buf));
                 select_buf_n = 0;
+                fmt_append_str("%s");
                 break;
             }
 
@@ -61,10 +94,7 @@ int main(int argc, char **argv) {
 
     if(state != START) error("Unmatched {{");
 
-    if(print_buf_n > 0){
-        BUF_APPEND(print_buf, '\0');
-        printf("print \"%s\"\n", print_buf);
-    }
+    fmt_dump();
 
     return 0;
 }
